173492deca62a3b9f5e2aedbf77485de4ecfea5e
Osama AlQudah <nightknight218@gmail.com>
2022-05-28 11:15:18 +0300

    Fully working program

--- pomo.py
+++ pomo.py
@@ -1,6 +1,25 @@
+import time
 import click
 
 
+def start_countdown(duration, minimal):
+    while duration:
+        mins, secs = divmod(duration, 60)
+        timeformat = f"{mins:02}:{secs:02}"
+        output = f"{timeformat}\r" if minimal else f"🍅 Time remaining - {timeformat}\r"
+        click.echo(output, nl=False)
+        time.sleep(1)
+        duration -= 1
+
+    print("Timer ended.")
+
+
 @click.command()
-def cli():
-    print("Hello, world!")
+@click.option("-d", "--duration", default=25, show_default=True, help="The timer duration in minutes")
+@click.option("-s", "--in-seconds", is_flag=True, default=False, show_default=True, help="Timer duration is provided in seconds")
+@click.option("-m", "--minimal", is_flag=True, default=False, show_default=True, help="Print minimal timer output")
+def cli(duration, in_seconds, minimal):
+    """Starts a pomodoro timer countdown."""
+    duration_in_seconds = duration if in_seconds else duration * 60
+
+    start_countdown(duration_in_seconds, minimal)
