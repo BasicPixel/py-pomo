c94a356e6efaf30c5e2dd485ee99987bfd65e2fb
Osama AlQudah <nightknight218@gmail.com>
2022-05-28 11:51:15 +0300

    Add license file, prepare for pypi distribution

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
 from setuptools import setup
+import pathlib
+
+# Get the project root directory
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="pomo",
+    name="py-pomo",
     version="1.0",
+    description="A simple CLI pomodoro timer",
+    long_description=long_description,
+    url="https://github.com/BasicPixel/py-pomo",
+    author="Osama AlQudah",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Topic :: Utilities"
+    ],
+    keywords="pomodoro, click, cli",
+    author_email="osama.mo.qudah@gmail.com",
     py_modules=["pomo"],
     install_requires=["click"],
     entry_points="""
